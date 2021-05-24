2c373e4f431e5d875fbfaa2f8d8f3709e625967a
AjayNaruka <naruka.ajay1997@gmail.com>
2021-05-24 18:45:28 +0200

    fine

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      <i class="fab fa-spotify"></i>
+    </div>
+    <div class="container">
+      <div class="row">
+        <Album 
+        v-for="(music,index) in musicArray"
+        :key="index"
+        :item=music
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios'
+import Album from '@/components/Album'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      axios,
+      musicArray:[]
+    }
+  },
+
+
   components: {
-    HelloWorld
+    Album
+  },
+  created(){
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(res => {
+      this.musicArray=res.data.response
+      console.log(res.data.response)
+    })
+    .catch(err => {
+      console.error(err); 
+    })
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import '@/assets/styles/general';
+body{
+  background-color:#1E2D3B ;
+}
+body .header{
+  background-color: #2E3A46;
+  height: 70px;
+  margin-bottom: 20px;
+  i{
+    line-height: 70px;
+    font-size: 38px;
+    margin-left: 10px;
+    color: white;
+    
+  }
 }
 </style>
---
+++ src/components/Album.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="album col-md-6 col-lg-2">
+      <div class="album-container">
+        <img class="img-fluid" :src="item.poster" alt="">
+      <h4 class="white">{{item.title.toUpperCase()}}</h4>
+      <h6>{{item.author}}</h6>
+      <h6>{{item.year}}</h6>
+      </div>
+      
+    </div>
+</template>
+
+<script>
+export default {
+name:'Album',
+props:{
+  item: Object
+}
+}
+</script>
+
+<style lang="scss" scoped>
+  .album{
+    display: flex;
+    flex-direction: column;
+    /* background: red; */
+    .album-container{
+      background: #2E3A46;
+      text-align: center;
+      min-height: 380px;
+      margin-bottom: 10px;
+      padding:5px 10px ;
+      color: grey;
+      img{
+        margin-top:10px ;
+      }
+      h4.white{
+        margin-top: 10px;
+        color: white;
+      }
+    }
+  }
+  
+  
+</style>
