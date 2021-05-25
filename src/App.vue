c5502af8dfc01048b9bf2e29eb1d2edee5d2f2fc
AjayNaruka <naruka.ajay1997@gmail.com>
2021-05-25 17:47:37 +0200

    fine

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@
     <div class="header">
       <i class="fab fa-spotify"></i>
     </div>
+    
     <div class="container">
+      <Search 
+      @ricerca='searching'
+      />
       <div class="row">
         <Album 
-        v-for="(music,index) in musicArray"
+        v-for="(music,index) in filterAlbums"
         :key="index"
         :item=music
         />
@@ -19,19 +23,30 @@
 
 import axios from 'axios'
 import Album from '@/components/Album'
+import Search from '@/components/Search'
 
 export default {
   name: 'App',
   data(){
     return{
       axios,
-      musicArray:[]
+      musicArray:[],
+      textToSearch:'',
+      filterValue:0
+    }
+  },
+  methods:{
+    searching(text,option){
+      this.textToSearch=text;
+      this.filterValue=option;
+      console.log('app filter:',this.filterValue);
     }
   },
 
 
   components: {
-    Album
+    Album,
+    Search
   },
   created(){
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
@@ -42,6 +57,13 @@ export default {
     .catch(err => {
       console.error(err); 
     })
+  },
+  computed:{
+    filterAlbums(){
+      if(this.textToSearch==='') return this.musicArray
+      if(this.filterValue===0) return this.musicArray.filter(item => item.genre.toLowerCase().includes(this.textToSearch.toLowerCase()))
+      return this.musicArray.filter(item => item[this.filterValue].toLowerCase().includes(this.textToSearch.toLowerCase()))
+    }
   }
 }
 </script>
---
+++ src/components/Search.vue
@@ -0,0 +1,64 @@
+<template>
+  <form class="d-flex">
+    <div class="flex-grow-1">
+      <input
+        class="form-control"
+        placeholder="Cerca"
+        v-model.trim=inputText
+        type="text">
+    </div>
+    <div>
+      <select name="filters" id="filters" class="my-select" @change="optionChange($event)">
+        <option >--scegli--</option>
+        <option value="genre">Genere</option>
+        <option value="author">Artista</option>
+      </select>
+      <button class="ms-3 btn btn-primary" @click.prevent="startSearch()">CERCA</button> <!-- PREVENT per evitare il reload della pagina dovuto dal form -->
+      <button class="ms-3 btn btn-warning" @click.prevent='resetSearch()'>RESET</button>
+      <!-- <label for="filters"></label> -->
+      
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name:'Search',
+  data(){
+    return{
+      inputText:'',
+      /* filterOption:this.optionChange() */
+      filterOption:0,
+    }
+  },
+  methods:{
+    resetSearch(){
+      console.log('reset');
+      this.inputText='';
+      this.startSearch();
+    },
+    startSearch(){
+      this.$emit('ricerca',this.inputText,this.filterOption  )
+    },
+    optionChange(event){
+      console.log(event.target.value);
+      this.filterOption=event.target.value
+      console.log('option: ', this.filterOption);
+      return event.target.value
+    }
+  },
+  
+}
+</script>
+
+<style>
+  input{
+    margin-bottom: 20px;
+  }
+  .my-select{
+    vertical-align: middle;
+   padding: 7px;
+   margin-left: 10px ;
+   border-radius: 5px;
+  }
+</style>
